e4e625e9b14f9253d88ce29e8a348218ce563de1
hafizdarmawan <hafizdarmawan53@gmail.com>
2021-11-04 13:15:03 +0700

    Mengubah model submission dan user serta menambah Admin/accountController

--- app/Submission.php
+++ app/Submission.php
@@ -6,5 +6,53 @@ use Illuminate\Database\Eloquent\Model;
 
 class Submission extends Model
 {
-    //
+    protected $fillable = ['number', 'letter_id', 'data'];
+
+    protected $casts = ['approval_at' => 'datetime'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function letter()
+    {
+        return $this->belongsTo('App\Letter');
+    }
+
+    public function admin()
+    {
+        return $this->belongsTo('App\Admin');
+    }
+
+    public function getStatus()
+    {
+        switch ($this->approval_status) {
+            case 0:
+                return "Menunggu Persetujuan";
+                break;
+            case 1:
+                return "Disetujui";
+                break;
+            case 2:
+                return "Ditolak";
+                break;
+            default:
+                return "Tidak Diketahui";
+                break;
+        }
+    }
+
+    public function getData($name)
+    {
+        $json = json_decode($this->data);
+        $data = [];
+        foreach ($json as $key=> $val) {
+            $data[$key] = $val;
+        }
+        return $data[$name];
+    }
+
+
+
 }
--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'photo', 'sin', 'name', 'password', 'birth_place', 'birth_date', 'gender', 'address', 'religion', 'marital_status', 'profession'
     ];
 
     /**
@@ -34,6 +34,16 @@ class User extends Authenticatable
      * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'birth_date' => 'date',
     ];
+
+    public function getShortGender()
+    {
+        return substr($this->gender, 0, 1);
+    }
+
+    public function getPsb()
+    {
+        return $this->birth_place . ', ' . $this->birth_date->formatLocalized('%d %B %Y');
+    }
 }
