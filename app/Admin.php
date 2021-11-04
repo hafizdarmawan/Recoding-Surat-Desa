98991719948bc645cd3190600c7d18e2bcbedb2d
hafizdarmawan <hafizdarmawan53@gmail.com>
2021-11-04 12:56:39 +0700

    Mengubah Model Admin

--- app/Admin.php
+++ app/Admin.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Admin extends Model
 {
-    //
+    use Notifiable;
+    protected $fillable = ['photo', 'name', 'username', 'password'];
+
+    protected $hidden = ['password', 'remember_token'];
 }
