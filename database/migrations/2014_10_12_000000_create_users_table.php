21c692259072e567ea8a2268d34d23a706cdf727
hafizdarmawan <hafizdarmawan53@gmail.com>
2021-11-04 12:54:38 +0700

    Memperbaiki Migration dan membuat semua model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('photo')->default('assets/images/users/user-1.jpg');
+            $table->string('sin')->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('birth_place')->nullable();
+            $table->date('birt_date')->nullable();
+            $table->enum('gender', ['Laki-Laki', 'Perempuan']);
+            $table->text('address')->nullable();
+            $table->enum('religion', ['Islam', 'Protestan', 'Katolik', 'Hindu', 'Buddha', 'Konghucu']);
+            $table->enum('marital_status', ['Belum Kawin', 'Kawin', 'Cerai']);
+            $table->string('phone_number')->nullable();
+            $table->string('professions')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
