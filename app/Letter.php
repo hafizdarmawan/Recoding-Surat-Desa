fbe39f1d3e2fa772e49a76795b8c51d8c76e675b
hafizdarmawan <hafizdarmawan53@gmail.com>
2021-11-04 12:59:12 +0700

    Mengubah Letter

--- app/Letter.php
+++ app/Letter.php
@@ -3,8 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Letter extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'content',
+        'data',
+        'status'
+    ];
+
+    public function status()
+    {
+        return $this->status == 'On' ? 'Aktif' : 'Tidak Aktif';
+    }
+
+    public function getData()
+    {
+        return json_decode($this->data);
+    }
 }
