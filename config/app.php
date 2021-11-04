45e012774ef738ee61c52d3e7456aa9d1ea18aef
hafizdarmawan <hafizdarmawan53@gmail.com>
2021-11-04 12:17:04 +0700

    Menambahkan require Yajra

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
         Waavi\Sanitizer\Laravel\SanitizerServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -237,6 +238,7 @@ return [
         'Image' => Intervention\Image\Facades\Image::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'Sanitizer' => Waavi\Sanitizer\Laravel\Facade::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
     ],
 
 ];
